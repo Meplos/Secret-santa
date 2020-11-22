17bb0fad388c8e0ec6ff1af892eab0e5f85bb4de
aerard <alexxd64150@gmail.com>
2020-11-22 22:26:30 +0100

    Change port

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
-const PORT = 8000;
+const PORT = process.env.port || 8000;
 const app = express();
 
 let people = [

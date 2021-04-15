c7073b9b07151fb9e78bdcbaa218c71934902e7d
Meplos <alexxd64150@gmail.com>
2021-04-15 08:05:08 +0200

    revert

---
+++ backend/src/index.js
@@ -0,0 +1,17 @@
+const app = require("express")();
+const ip = require("ip");
+
+const PORT = 3000 || process.env.PORT;
+
+app.get('/', (req, res) => {
+    res.status(200).json({message: "Hello world 🖖"});
+});
+
+
+app.listen(PORT, () => {
+    console.log('🖥️  Server listening... ');
+    console.log(`\t 🏠 : http://localhost:${PORT}`);
+    console.log(`\t 🌍 : http://${ip.address()}:${PORT}`);
+})
+
+
